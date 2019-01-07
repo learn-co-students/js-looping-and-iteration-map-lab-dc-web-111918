547a40145da064b086561c3476fa1969bab7d29f
github username <github email address>
2019-01-07 12:17:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solution in this file.
-lowerCaseDrivers(arr) {
+function lowerCaseDrivers(arr) {
     arr.map(function(drivers){ return drivers.toLowerCase(); });
 };
 
 //[john smith, sally jones, beau jepson]
-nameToAttributes(arr) {
+function nameToAttributes(arr) {
   arr.map(function(drivers){
     let splitName = drivers.split(" ");
     return { firstName: splitName[0], lastName: splitName[1] };
@@ -13,7 +13,7 @@ nameToAttributes(arr) {
 
 };
 
-attributesToPhrase(arr) {
+function attributesToPhrase(arr) {
   arr.map(function(drivers){
 
     return `${drivers.name} is from ${drivers.hometown}`;
